0f30a3aab52f729c8a58a26b370cd20c9a0d0213
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-11-03 12:19:25 +0330

    tailwind added to project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h3>cmy app</h3>
+      <h1 className="text-3xl font-bold underline bg-red-100">cmy app</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
